808f891dfe770a5b22446cc67583dea4a02a612c
eugenekoval <evgenykoval@gmail.com>
2013-01-16 10:08:22 +0200

    New Structure

---
+++ frontend/js/settings.js
@@ -0,0 +1,11 @@
+var settings = {
+    prefix: 'threesixty_',  //Images Prefix
+    imageDir: 'images/content/',      //Images Path
+    extension: 'jpg',       //Images extension
+    speedMultiplier : 10,   //Speed of play
+    totalFrames: 65,        //Count of pictures
+    startImage: 1,          //Number of start image
+    direction: 1            //Direction 1-right, 0-left
+};
+
+
